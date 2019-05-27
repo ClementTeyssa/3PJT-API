16a8e46ba4bcfff520574b35398516e8629da663
clement <292621@supinfo.com>
2019-05-27 10:22:58 +0200

    Add transactions model

--- config/database.go
+++ config/database.go
@@ -19,11 +19,27 @@ func DatabaseInit() {
 		log.Panic(err)
 	}
 	createUsersTable()
+	createTransactionsTable()
+	createBlocksTable()
 }
 
 // Create Table users if not exists
 func createUsersTable() {
 	_, err := db.Exec("CREATE TABLE IF NOT EXISTS users(id serial, email varchar(100), password varchar, adress varchar, privatekey bytea, created_at timestamp default NULL, updated_at timestamp default NULL, constraint pk primary key(id))")
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func createTransactionsTable() {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS transactions(id serial, accountfrom varchar, accountto varchar, amount float, created_at timestamp default NULL, updated_at timestamp default NULL, constraint pk primary key(id))")
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func createBlocksTable() {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS blocks(id serial, timestamp int, transactionid int, hash varchar, prevhash varchar, created_at timestamp default NULL, updated_at timestamp default NULL, constraint pk primary key(id))")
 	if err != nil {
 		log.Panic(err)
 	}
---
+++ models/transactions.go
@@ -0,0 +1,81 @@
+package models
+
+import (
+	"log"
+	"time"
+
+	"github.com/ClementTeyssa/3PJT-API/config"
+)
+
+type Transaction struct {
+	ID          int       `json:"id" validate:"omitempty,uuid"`
+	AccountFrom string    `json:"accountfrom" validate:"required"`
+	AccountTo   string    `json:"accountto" validate:"required"`
+	Amount      float32   `json:"amount" validate:"required"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type Transactions []Transaction
+
+func NewTransaction(transaction *Transaction) {
+	if transaction == nil {
+		log.Panic(transaction)
+	}
+	transaction.CreatedAt = time.Now()
+	transaction.UpdatedAt = time.Now()
+	err := config.GetDb().QueryRow("INSERT INTO transactions (accountfrom, accountto, amount, created_at, updated_at) VALUES ($1,$2,$3,$4,$5) RETURNING id;", transaction.AccountFrom, transaction.AccountTo, transaction.Amount, transaction.CreatedAt, transaction.UpdatedAt).Scan(&transaction.ID)
+
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func FindTransactionById(id int) *Transaction {
+	var transaction Transaction
+	row := config.GetDb().QueryRow("SELECT * FROM transactions WHERE id = $1;", id)
+	err := row.Scan(&transaction.ID, &transaction.AccountFrom, &transaction.AccountTo, &transaction.Amount, &transaction.CreatedAt, &transaction.UpdatedAt)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &transaction
+}
+
+func AllTransactions() *Transactions {
+	var transactions Transactions
+	rows, err := config.GetDb().Query("SELECT * FROM transactions")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var transaction Transaction
+		err := rows.Scan(&transaction.ID, &transaction.AccountFrom, &transaction.AccountTo, &transaction.Amount, &transaction.CreatedAt, &transaction.UpdatedAt)
+		if err != nil {
+			log.Panic(err)
+		}
+		transactions = append(transactions, transaction)
+	}
+	return &transactions
+}
+
+func UpdateTransaction(transaction *Transaction) {
+	transaction.UpdatedAt = time.Now()
+	stmt, err := config.GetDb().Prepare("UPDATE transactions SET accountfrom=$1, accountto=$2, amount=$3, updated_at=$4 WHERE id=$5;")
+	if err != nil {
+		log.Panic(err)
+	}
+	_, err = stmt.Exec(transaction.AccountFrom, transaction.AccountTo, transaction.Amount, transaction.UpdatedAt)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func DeleteTransactionById(id int) error {
+	stmt, err := config.GetDb().Prepare("DELETE FROM transactions WHERE id=$1;")
+	if err != nil {
+		log.Panic(err)
+	}
+	_, err = stmt.Exec(id)
+	return err
+}
